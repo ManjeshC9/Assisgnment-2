563e8b8c87ae4167e298cf3d6eebd11f4a22ce12
ManjeshC9 <120353457+ManjeshC9@users.noreply.github.com>
2022-12-22 11:13:44 +0530

    q2.py

---
+++ q2.py
@@ -0,0 +1,6 @@
+name ='Manjesh Chauhan'
+sid = '22105111'
+depatment_name = 'ECE'
+cgpa = '10'
+# printing a para using different variables.
+print(f"Hey,{name} Here! \nMy SID is {sid} \nI am from {depatment_name} department and my CGPA is {cgpa}")
